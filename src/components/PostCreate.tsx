c16e0348ce35e82f1ae7ec20fb71406458ddaede
Gareth <garethdavisrogers@gmail.com>
2025-12-14 16:26:15 -0500

    Set posts to receive input.  Needs API hookup

--- src/components/PostCreate.tsx
+++ src/components/PostCreate.tsx
@@ -1,19 +1,23 @@
+import { useState } from 'react';
 import '../app/App.css'
 import type {Post} from '../types/features/Post';
 
 export function PostCreate() {
 
-  return (
-    <>
-    <form>
-        <fieldset>
-            <h1>Post Something</h1>
-            <input type="text"></input>
-            <button type="submit">Log In</button>
-        </fieldset>
-    </form>
-    </>
-  )
+    const [body, setBody] = useState<string>("");
+    return (
+        <>
+        <form>
+            <fieldset>
+                <div className="form-group">
+                    <label>Post</label>
+                    <input type="textarea" placeholder='post' value={body} onChange={(e)=>{setBody(e.target.value)}}/>
+                </div>
+                <button type="submit">Post</button>
+            </fieldset>
+        </form>
+        </>
+    )
 }
 
 export default PostCreate
